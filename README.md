392cb72b4ccba5ab5b727590ac48c9b56797d054
kulotsystems <kulotsystems@gmail.com>
2022-04-16 14:56:59 +0800

    Lesson 2: Compiling TypeScript

---
+++ README.md
@@ -0,0 +1,2 @@
+## Typescript Tutorial by TheNetNinja
+This project is built from [TypeScript Tutorial by **TheNetNinja**](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gUgr39Q_yD6v-bSyMwKPUI).
---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'luigi';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
