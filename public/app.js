64dcfd4786637f555478b44ebdcd6bc6d328dd6c
kulotsystems <kulotsystems@gmail.com>
2022-04-16 21:00:00 +0800

    Lesson 18: Generics

--- public/app.js
+++ public/app.js
@@ -21,3 +21,24 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
+// GENERICS
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+// let docTwo = addUID('hello');
+console.log(docOne.age);
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: {
+        name: 'shaun'
+    }
+};
+const docFour = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'toilet roll']
+};
+console.log(docFour);
--- src/app.ts
+++ src/app.ts
@@ -27,3 +27,39 @@ form.addEventListener('submit', (e: Event) => {
 
     list.render(doc, type.value, 'end');
 });
+
+
+// GENERICS
+const addUID = <T extends {name: string}>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+// let docTwo = addUID('hello');
+
+console.log(docOne.age);
+
+
+// with interfaces
+interface Resource<T> {
+    uid: number,
+    resourceName: string,
+    data: T
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: {
+        name: 'shaun'
+    }
+};
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'toilet roll']
+}
+
+console.log(docFour);
