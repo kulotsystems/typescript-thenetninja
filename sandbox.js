f39d82f5ca27ec4192f6953a7b9828e29504da3b
kulotsystems <kulotsystems@gmail.com>
2022-04-16 15:12:14 +0800

    Lesson 3: Type Basics

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,13 @@
-var character = 'luigi';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = 'luigi';
+// age = 'yoshi';
+age = 40;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,18 @@
-const character = 'luigi';
-console.log(character);
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
 
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
+// character = 20;
+character = 'luigi';
 
-inputs.forEach(input => {
-    console.log(input);
-});
+// age = 'yoshi';
+age = 40;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ(7.5);
