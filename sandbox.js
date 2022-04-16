a8d0cbb46087b3bb6a60216daab167e7feaa3d8b
kulotsystems <kulotsystems@gmail.com>
2022-04-16 15:20:56 +0800

    Lesson 4: Objects and Arrays

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,30 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 20;
-character = 'luigi';
-// age = 'yoshi';
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+// names = 'hello';
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[1] = 'shaun';
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+/* allowed but do not remove or add new property: */
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,38 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+// names = 'hello';
 
-// character = 20;
-character = 'luigi';
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
 
-// age = 'yoshi';
-age = 40;
+let numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[1] = 'shaun';
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
-}
 
-console.log(circ(7.5);
+// objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age : 30
+};
+
+ninja.age  = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+
+
+/* allowed but do not remove or add new property: */
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age : 40
+};
