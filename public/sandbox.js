57011da46497d59f951f35d9a6095354775062d4
kulotsystems <kulotsystems@gmail.com>
2022-04-16 16:49:45 +0800

    Lesson 8: Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('mario');
+let greet;
+// greet = 'hello';
+greet = () => {
+    console.log('hello, again');
+};
+// const add = (a: number, b: number, c?: number | string) => {
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
+// result = 'something else';
