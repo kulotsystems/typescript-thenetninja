df6cff66d9e85e27c19bea4d35684211a555f259
kulotsystems <kulotsystems@gmail.com>
2022-04-16 20:44:34 +0800

    Lesson 17: Rendering an HTML Template

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,11 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-//
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-//
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-//
-// console.log(docs);
-
-// const invOne = new Invoice('Mario', 'work on the mario website', 250);
-// const invTwo = new Invoice('Luigi', 'work on the luigi website', 300);
-//
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-//
-// invoices.forEach(inv => {
-//     console.log(inv.client, inv.amount, inv.format());
-// });
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -43,5 +25,5 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
     }
 
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
