e1edb5a75b250b49ac4c4ee03f6d0b48cf8af8db
kulotsystems <kulotsystems@gmail.com>
2022-04-16 20:05:59 +0800

    Lesson 15: Interfaces

--- public/app.js
+++ public/app.js
@@ -1,3 +1,19 @@
+const me = {
+    name: 'shaun',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log('Hello', person.name);
+};
+greetPerson(me);
+console.log(me);
 import { Invoice } from './classes/Invoice.js';
 const invOne = new Invoice('Mario', 'work on the mario website', 250);
 const invTwo = new Invoice('Luigi', 'work on the luigi website', 300);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,33 @@
+// interfaces
+interface IsPerson {
+    name: string;
+    age : number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: 'shaun',
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+
+const greetPerson = (person: IsPerson) => {
+    console.log('Hello', person.name);
+}
+
+greetPerson(me);
+console.log(me);
+
+
+
+
 import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('Mario', 'work on the mario website', 250);
